8bc9c0cf6e46f03836043dedb33988ef425b60eb
Yaroslav Kabay <yakabay86@gmail.com>
2018-10-06 14:08:29 +0300

    Make base layout

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
 	<div id="app">
-		<header>
-
+		<header class="header">
+			<div class="container">
+				<div class="row center-xs" style="height: 100%">
+					<div class="col-xs-11 col-xl-9" style="height: 100%">
+						<div class="header__container" style="height: 100%">
+							<router-link to="/">Home</router-link>
+							<router-link to="/cart">Cart</router-link>
+						</div>
+					</div>
+				</div>
+			</div>
+			
 		</header>
 
-		<div id="nav">
-			<router-link to="/">Home</router-link> |
-			<router-link to="/about">About</router-link>
-		</div>
-		<router-view/>
+		<main class="container">
+			<div class="row center-xs" style="height: 100%">
+				<div class="col-xs-11 col-xl-9" style="height: 100%">
+					<div class="card">
+						<router-view/>
+					</div>
+				</div>
+			</div>
+		</main>
+		
 	</div>
 </template>
 
@@ -17,24 +32,51 @@
 	font-family: 'Avenir', Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
+	min-height: 100vh;
+	background-color: #E6E9EF;
 	color: #2c3e50;
 }
 
-header {
-	background: blue;
-	height: 4rem;
+
+.header {
+	background: white;
+	height: 3.825rem;
+	&__container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
 }
 
+.card {
+	background-color: white;
+	margin-top: 3.6rem;
+	border-radius: 6px;
+	box-shadow: 0px 2px 20px 4px rgba(221,221,221,1);
+}
 
-#nav {
-	padding: 30px;
-	a {
-		font-weight: bold;
-		color: #2c3e50;
-		&.router-link-exact-active {
-		color: #42b983;
-		}
+.container {
+	height: 100%;
+	max-width: 100%;
+}
+@media only screen and (min-width: 576px) {
+	.container {
+		width: 560px;
+	}
+}
+@media only screen and (min-width: 768px) {
+	.container {
+		width: 752px;
+	}
+}
+@media only screen and (min-width: 992px) {
+	.container {
+		width: 976px;
+	}
+}
+@media only screen and (min-width: 1200px) {
+	.container {
+		width: 1184px;
 	}
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  	<div class="home">
+		<dir>
+			<img alt="Vue logo" src="../assets/logo.png">
+			<h2>This test app is made with Vue.js</h2>
+			<h3>Please navigate to <router-link to="/cart">Cart</router-link> to play with it )</h3>
+		</dir>
+		
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+ 	</div>
 </template>
 
 <script>
@@ -10,9 +15,18 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+	name: 'home',
+	components: {
+			HelloWorld
+	}
 }
 </script>
+
+<style lang="scss">
+	.home {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 30rem;
+	}
+</style>
